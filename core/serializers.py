49ec40f9177b9985d7d5f7e2aaf7297ed61fa736
Gethol <natenderitu@gmail.com>
2024-09-02 13:53:47 +0300

    feat: create a base serializer to allow for soft deletes

--- core/serializers.py
+++ core/serializers.py
@@ -1,20 +1,24 @@
 from .models import *
 from rest_framework import serializers
 
-class UserSerializer(serializers.ModelSerializer):
+
+class SoftDeleteMixin:
+    def perform_soft_delete(self, instance):
+        instance.delete()  # Call the soft delete method from the model
+class UserSerializer(serializers.ModelSerializer, SoftDeleteMixin):
     class Meta:
         model = User
-        fields = ['id','email', 'first_name', 'last_name', 'other_names', 'phone_number', 'address', 'dob' ]
+        fields = ['id','email','password', 'first_name', 'last_name', 'other_names', 'phone_number', 'address', 'dob', 'is_staff', 'is_superuser', 'is_active' ]
         extra_kwargs = {'password': {'write_only': True}}
 
-class EmployeeSerializer(serializers.ModelSerializer):
+class EmployeeSerializer(serializers.ModelSerializer, SoftDeleteMixin):
     user = UserSerializer()
 
     class Meta:
         model = Employee
         fields = ['id','user', 'instance', 'entity', 'unit']
 
-class AdminSerializer(serializers.ModelSerializer):
+class AdminSerializer(serializers.ModelSerializer, SoftDeleteMixin):
     user = UserSerializer()
     admin_type = serializers.SlugRelatedField(slug_field='name', queryset=AdminType.objects.all()) 
 
