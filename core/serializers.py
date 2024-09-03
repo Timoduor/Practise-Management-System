f359a03b059434a5f9ee5d624fc86cf741fd1f2c
Gethol <natenderitu@gmail.com>
2024-09-03 15:19:25 +0300

    feat: implement creation of employee simultaneously with user

--- core/serializers.py
+++ core/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 
 
 class SoftDeleteMixin:
-    def perform_soft_delete(self, instance):
+    def delete(self, instance):
         instance.delete()  # Call the soft delete method from the model
 
 class UserSerializer(serializers.ModelSerializer, SoftDeleteMixin):
@@ -11,18 +11,42 @@ class UserSerializer(serializers.ModelSerializer, SoftDeleteMixin):
 
     # admin = serializers.PrimaryKeyRelatedField(queryset=Admin.objects.all(), required=False)
     # employee = serializers.PrimaryKeyRelatedField(queryset=Employee.objects.all(), required=False)
-    # employee_instance = serializers.PrimaryKeyRelatedField(queryset=Instance.objects.all(),  required=False)
-    # employee_entity = serializers.PrimaryKeyRelatedField(queryset=Entity.objects.all(),  required=False)
-    # employee_unit = serializers.PrimaryKeyRelatedField(queryset=Unit.objects.all(), required=False)
+    employee_instance = serializers.PrimaryKeyRelatedField(queryset=Instance.objects.all(),  required=False)
+    employee_entity = serializers.PrimaryKeyRelatedField(queryset=Entity.objects.all(),  required=False)
+    employee_unit = serializers.PrimaryKeyRelatedField(queryset=Unit.objects.all(), required=False)
     class Meta:
         model = User
         fields = ['id','email','password', 'first_name', 'last_name', 'other_names', 'phone_number', 'address', 'dob', 'is_staff', 'is_superuser', 'is_active', 'employee_instance', 'employee_entity', 'employee_unit' ]
         extra_kwargs = {'password': {'write_only': True},
                     }
-    
+        
+    def validate(self, data):
+        # Get the values from the serializer data
+        employee_instance = data.get('employee_instance')
+        employee_entity = data.get('employee_entity')
+        employee_unit = data.get('employee_unit')
+
+        # Validate employee_entity against employee_instance
+        if employee_entity and employee_instance:
+            if employee_entity.instance != employee_instance:
+                raise serializers.ValidationError({
+                    'employee_entity': 'The selected entity must belong to the selected instance.'
+                })
+
+        # Validate employee_unit against employee_entity
+        if employee_unit and employee_entity:
+            if employee_unit.entity != employee_entity:
+                raise serializers.ValidationError({
+                    'employee_unit': 'The selected unit must belong to the selected entity.'
+                })
+
+        return data
+
     def create(self, validated_data):
         admin_data = validated_data.pop('admin',None)
-        employee_data = validated_data.pop('employee', None)
+        employee_instance = validated_data.pop('employee_instance', None)
+        employee_entity = validated_data.pop('employee_entity', None)
+        employee_unit = validated_data.pop('employee_unit', None)
         user = User.objects.create(**validated_data)
         user.set_password(validated_data['password'])
         user.save()
@@ -34,9 +58,9 @@ class UserSerializer(serializers.ModelSerializer, SoftDeleteMixin):
         if 'employee_instance' in validated_data:
             Employee.objects.create(
                 user = user,
-                instance = validated_data['employee_instance'],
-                entity = validated_data.get("employee_entity"),
-                unit = validated_data.get("employee_unit")
+                instance = employee_instance,
+                entity = employee_entity,
+                unit = employee_unit
 
             )
         return user
@@ -71,7 +95,10 @@ class EntitySerializer(serializers.ModelSerializer, SoftDeleteMixin):
 
 class InstanceSerializer(serializers.ModelSerializer, SoftDeleteMixin):
     entities = EntitySerializer(many=True, read_only=True)
+
     class Meta:
         model = Instance
         fields = ['id' , 'name', 'code' ,'industry', 'is_deleted' ,'created_at', 'updated_at', 'last_updated_by', 'created_by', 'entities']
     
+
+
