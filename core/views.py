9bd83e93834b4f1b3c4c96d16f39932f25680b6e
Nathan Nderitu <nathan.nderitu@sbsl.co.ke>
2024-09-04 12:13:10 +0000

    Revert "Revert "feat: create viewsets for the admins, employees, instances, entities and units""

--- core/views.py
+++ core/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import *
+from .serializers import *
+from rest_framework import generics,viewsets, status
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
