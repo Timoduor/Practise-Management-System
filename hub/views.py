e434df16701767d9c80d78d87a47c01e29e40954
Kendi Njeru <kendi.njeru@strathmore.edu>
2024-09-09 09:12:25 +0300

    feat:create viewsets for projects, tasks, invoices

--- hub/views.py
+++ hub/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Customer, Contact, Sales
-from .serializers import CustomerSerializer, ContactSerializer, SalesSerializer
+from .models import Customer, Contact, Sales, Project, Task, Invoice
+from .serializers import CustomerSerializer, ContactSerializer, SalesSerializer, ProjectSerializer, TaskSerializer, InvoiceSerializer
 
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
@@ -15,3 +15,14 @@ class SalesViewSet(viewsets.ModelViewSet):
     queryset = Sales.objects.all()
     serializer_class = SalesSerializer
 
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+class InvoiceViewSet(viewsets.ModelViewSet):
+    queryset = Invoice.objects.all()
+    serializer_class = InvoiceSerializer
