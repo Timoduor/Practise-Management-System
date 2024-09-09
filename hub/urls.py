6576d3d93afe032b996f90bf8f0e65511f4ccd7c
Kendi Njeru <kendi.njeru@strathmore.edu>
2024-09-09 09:13:37 +0300

    feat: update urls for project, tasks, invoices

--- hub/urls.py
+++ hub/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CustomerViewSet, ContactViewSet, SalesViewSet
+from .views import CustomerViewSet, ContactViewSet, SalesViewSet, ProjectViewSet, TaskViewSet, InvoiceViewSet
 
 router = DefaultRouter()
 router.register(r'customers', CustomerViewSet)
 router.register(r'contacts', ContactViewSet)
 router.register(r'sales', SalesViewSet)
+router.register(r'projects', ProjectViewSet)
+router.register(r'tasks', TaskViewSet)
+router.register(r'invoices', InvoiceViewSet)
 
 urlpatterns = router.urls
