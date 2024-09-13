d220406132f52536819a946e236396e80fc18f49
Kendi Njeru <kendi.njeru@strathmore.edu>
2024-09-13 10:09:11 +0300

    chore: add urls for timesheet models

--- hub/urls.py
+++ hub/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CustomerViewSet, ContactViewSet, ProjectViewSet, TaskViewSet, SalesViewSet, InvoiceViewSet, ProjectPhaseViewSet, WorkEntriesViewSet, AbsenceViewSet, ExpenseViewSet
+from .views import CustomerViewSet, ContactViewSet, ProjectViewSet, TaskViewSet, SalesViewSet, InvoiceViewSet, ProjectPhaseViewSet, WorkEntriesViewSet,LeaveTypeViewSet, AbsenceViewSet, ExpenseViewSet
 
 router = DefaultRouter()
 router.register(r'customers', CustomerViewSet)
@@ -11,6 +11,7 @@ router.register(r'phases', ProjectPhaseViewSet)
 router.register(r'tasks', TaskViewSet)
 router.register(r'invoices', InvoiceViewSet)
 router.register(r'work-entries', WorkEntriesViewSet)
+router.register(r'leavetypes', LeaveTypeViewSet)
 router.register(r'absences', AbsenceViewSet)
 router.register(r'expenses', ExpenseViewSet)
 
