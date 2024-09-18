b8c47fbd2216a94df6b6cd418c0ccf2658b6e800
Gethol <natenderitu@gmail.com>
2024-09-18 12:25:52 +0300

    feat: add nested serializers for projects and phases

--- hub/serializers.py
+++ hub/serializers.py
@@ -103,19 +103,62 @@ class SalesSerializer(serializers.ModelSerializer):
 
 #         return data
 
+class TaskSerializer(serializers.ModelSerializer):
+    project = serializers.PrimaryKeyRelatedField(queryset=Project.objects.all())
+    assigned_to = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), required=False)
+
+    class Meta:
+        model = Task
+        fields = ['task_id', 'task_name', 'project', 'phase', 'assigned_to', 'start_date', 'due_date', 'task_description', 'task_status', 'is_deleted', 'created_at', 'updated_at']
+
+    def validate(self, data):
+        start_date = data.get('start_date')
+        due_date = data.get('due_date')
+
+        if start_date and due_date and start_date > due_date:
+            raise serializers.ValidationError("Due date must be after the start date.")
+
+        project = data.get('project')
+        phase = data.get('phase')
+
+        if phase and phase.project != project:
+            raise serializers.ValidationError("The phase must belong to the selected project.")
+
+        return data
+
+
+class ProjectPhaseSerializer(serializers.ModelSerializer):
+    project = serializers.PrimaryKeyRelatedField(queryset=Project.objects.all())
+    tasks = TaskSerializer(many=True, read_only= True)
+
+    class Meta:
+        model = ProjectPhase
+        fields = ['phase_id', 'phase_name', 'project', 'phase_description', 'start_date', 'end_date', 'is_deleted', 'created_at', 'updated_at', 'tasks']
+
+    def validate(self, data):
+        start_date = data.get('start_date')
+        end_date = data.get('end_date')
+
+        if start_date and end_date and start_date > end_date:
+            raise serializers.ValidationError("End date must be after the start date.")
+
+        return data
+
+
 class ProjectSerializer(serializers.ModelSerializer):
     customer = serializers.PrimaryKeyRelatedField(queryset=Customer.objects.all())
     members = EmployeeSerializer(many=True, read_only=True)
     member_ids = serializers.PrimaryKeyRelatedField(
         many=True,  queryset=Employee.objects.all(), source='members'
     )
+    phases = ProjectPhaseSerializer(many=True, read_only=True)
 
     class Meta:
         model = Project
         fields = [
             'project_id', 'project_name', 'customer', 'project_description', 
             'start_date', 'end_date', 'entity', 'unit', 'is_deleted', 
-            'created_at', 'updated_at', 'members', 'member_ids'
+            'created_at', 'updated_at','members', 'member_ids', 'phases'
         ]
 
     def validate(self, data):
@@ -161,45 +204,7 @@ class ProjectSerializer(serializers.ModelSerializer):
         return instance
 
 
-class ProjectPhaseSerializer(serializers.ModelSerializer):
-    project = serializers.PrimaryKeyRelatedField(queryset=Project.objects.all())
 
-    class Meta:
-        model = ProjectPhase
-        fields = ['phase_id', 'phase_name', 'project', 'phase_description', 'start_date', 'end_date', 'is_deleted', 'created_at', 'updated_at']
-
-    def validate(self, data):
-        start_date = data.get('start_date')
-        end_date = data.get('end_date')
-
-        if start_date and end_date and start_date > end_date:
-            raise serializers.ValidationError("End date must be after the start date.")
-
-        return data
-
-
-class TaskSerializer(serializers.ModelSerializer):
-    project = serializers.PrimaryKeyRelatedField(queryset=Project.objects.all())
-    assigned_to = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), required=False)
-
-    class Meta:
-        model = Task
-        fields = ['task_id', 'task_name', 'project', 'phase', 'assigned_to', 'start_date', 'due_date', 'task_description', 'task_status', 'is_deleted', 'created_at', 'updated_at']
-
-    def validate(self, data):
-        start_date = data.get('start_date')
-        due_date = data.get('due_date')
-
-        if start_date and due_date and start_date > due_date:
-            raise serializers.ValidationError("Due date must be after the start date.")
-
-        project = data.get('project')
-        phase = data.get('phase')
-
-        if phase and phase.project != project:
-            raise serializers.ValidationError("The phase must belong to the selected project.")
-
-        return data
 
 
 class WorkEntriesSerializer(serializers.ModelSerializer):
