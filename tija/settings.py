890eb9e4df643efe364dd616b114154893b378f9
Gethol <natenderitu@gmail.com>
2024-09-19 09:50:26 +0300

    fix: reduce the token expiry time

--- tija/settings.py
+++ tija/settings.py
@@ -147,7 +147,7 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
   # It will work instead of the default serializer(TokenObtainPairSerializer).
   "TOKEN_OBTAIN_SERIALIZER": "core.serializers.CustomTokenObtainPairSerializer",
-  'ACCESS_TOKEN_LIFETIME': timedelta(minutes=180),
+  'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
   'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
   'ROTATE_REFRESH_TOKENS': True,
   'BLACKLIST_AFTER_ROTATION': True,
