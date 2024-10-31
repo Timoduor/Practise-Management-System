58009d09a8e54ec217ebb22da13b3fbde40d876b
Gethol <natenderitu@gmail.com>
2024-10-31 10:42:37 +0300

    fix: adjust length of industry name and instance name fields

--- core/models.py
+++ core/models.py
@@ -59,8 +59,8 @@ class Instance(SoftDeleteModel):
     INDUSTRY_CHOICES = []  # Placeholder for industry choices
 
     name = models.CharField(max_length=30)  # Instance name
-    code = models.CharField(max_length=10)  # Short code for instance
-    industry = models.CharField(max_length=15)  # Industry type
+    code = models.CharField(max_length=20)  # Short code for instance
+    industry = models.CharField(max_length=30)  # Industry type
 
     def __str__(self):
         return self.name  # String representation of the instance
