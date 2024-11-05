da24e7cf59f6fd8da5e7f9eab172047911349753
Gethol <natenderitu@gmail.com>
2024-11-05 12:22:13 +0300

    feat: set up custom user and employee fetching based on user status

--- core/views.py
+++ core/views.py
@@ -15,6 +15,66 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()  # Retrieve all User objects
     serializer_class = UserSerializer  # Use UserSerializer for serialization
 
+
+    def get_queryset(self):
+        user = self.request.user
+
+        # Check if the user is staff and has an admin profile
+        if user.is_staff and hasattr(user, 'admin_user'):
+            admin_user = user.admin_user
+            admin_type_name = admin_user.admin_type.name
+            jurisdiction = admin_user.jurisdiction  # Can be Instance, Entity, or Unit
+
+            if admin_type_name == "SUP":
+                # Superuser admin - return all users
+                return User.objects.all()
+            elif admin_type_name == "INS" and isinstance(jurisdiction, Instance):
+                # Instance admin - return users in the same instance
+                instance = jurisdiction
+                return User.objects.filter(
+                    models.Q(employee_user__instance=instance) |
+                    models.Q(admin_user__jurisdiction_content_type=ContentType.objects.get_for_model(Instance),
+                      admin_user__jurisdiction_object_id=instance.id)
+                ).distinct()
+            elif admin_type_name == "ENT" and isinstance(jurisdiction, Entity):
+                # Entity admin - return users in the same entity and child entities
+                entity = jurisdiction
+                # Get all descendant entities
+                all_entities = self.get_all_entities(entity)
+                entity_ids = [e.id for e in all_entities]
+
+                return User.objects.filter(
+                    models.Q(employee_user__entity__in=all_entities) |
+                    models.Q(admin_user__jurisdiction_content_type=ContentType.objects.get_for_model(Entity),
+                      admin_user__jurisdiction_object_id__in=entity_ids)
+                ).distinct()
+            elif admin_type_name == "UNI" and isinstance(jurisdiction, Unit):
+                # Unit admin - return users in the same unit
+                unit = jurisdiction
+                return User.objects.filter(
+                    models.Q(employee_user__unit=unit) |
+                    models.Q(admin_user__jurisdiction_content_type=ContentType.objects.get_for_model(Unit),
+                      admin_user__jurisdiction_object_id=unit.id)
+                ).distinct()
+            else:
+                return User.objects.none()
+        else:
+            # Non-staff users can only see themselves
+            return User.objects.filter(pk=user.pk)
+
+    def get_all_entities(self, parent_entity):
+        # Recursive function to get all descendant entities
+        entities = [parent_entity]
+        child_entities = Entity.objects.filter(parent_entity=parent_entity)
+        for child in child_entities:
+            entities.extend(self.get_all_entities(child))
+        return entities
+
+# Define a viewset for managing Admin objects
+class AdminViewSet(viewsets.ModelViewSet):
+    queryset = Admin.objects.all()
+    serializer_class = AdminSerializer
+
     def create(self, request):
         # Handle user creation with validation and response
         serializer = self.get_serializer(data=request.data)
@@ -23,14 +83,61 @@ class UserViewSet(viewsets.ModelViewSet):
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
-        # Uncommented code for setting up staff users as admins is provided but not active
-        # This would automatically assign an admin type if the user is staff
+    def get_queryset(self):
+        user = self.request.user
+
+        # Check if the user is staff and has an admin profile
+        if user.is_staff and hasattr(user, 'admin_user'):
+            admin_user = user.admin_user
+            admin_type_name = admin_user.admin_type.name
+            jurisdiction = admin_user.jurisdiction  # Can be Instance, Entity, or Unit
+
+            if admin_type_name == "SUP":
+                # Superuser admin - return all admins
+                return Admin.objects.all()
+            elif admin_type_name == "INS" and isinstance(jurisdiction, Instance):
+                # Instance admin - return admins in the same instance
+                instance = jurisdiction
+                return Admin.objects.filter(
+                    models.Q(jurisdiction_content_type=ContentType.objects.get_for_model(Instance),
+                      jurisdiction_object_id=instance.id) |
+                    models.Q(user__employee_user__instance=instance)
+                ).distinct()
+            elif admin_type_name == "ENT" and isinstance(jurisdiction, Entity):
+                # Entity admin - return admins in the same entity and child entities
+                entity = jurisdiction
+                # Get all descendant entities
+                all_entities = self.get_all_entities(entity)
+                entity_ids = [e.id for e in all_entities]
+
+                return Admin.objects.filter(
+                    models.Q(jurisdiction_content_type=ContentType.objects.get_for_model(Entity),
+                      jurisdiction_object_id__in=entity_ids) |
+                    models.Q(user__employee_user__entity__in=all_entities)
+                ).distinct()
+            elif admin_type_name == "UNI" and isinstance(jurisdiction, Unit):
+                # Unit admin - return admins in the same unit
+                unit = jurisdiction
+                return Admin.objects.filter(
+                    models.Q(jurisdiction_content_type=ContentType.objects.get_for_model(Unit),
+                      jurisdiction_object_id=unit.id) |
+                    models.Q(user__employee_user__unit=unit)
+                ).distinct()
+            else:
+                return Admin.objects.none()
+        else:
+            # Non-staff users should not see any admins
+            return Admin.objects.none()
+
+    def get_all_entities(self, parent_entity):
+        # Recursive function to get all descendant entities
+        entities = [parent_entity]
+        child_entities = Entity.objects.filter(parent_entity=parent_entity)
+        for child in child_entities:
+            entities.extend(self.get_all_entities(child))
+        return entities
 
 # Define a viewset for managing Admin objects
-class AdminViewSet(viewsets.ModelViewSet):
-    queryset = Admin.objects.all()  # Retrieve all Admin objects
-    serializer_class = AdminSerializer  # Use AdminSerializer for serialization
-
 # Define a viewset for managing Employee objects
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()  # Retrieve all Employee objects
