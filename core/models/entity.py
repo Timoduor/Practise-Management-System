722a1eb6a01a82fe8a58b3c7e0c62ddb80260f36
Gethol <natenderitu@gmail.com>
2024-11-11 16:06:39 +0300

    fix: rename the model imports

--- core/models/entity.py
+++ core/models/entity.py
@@ -10,9 +10,9 @@ class Entity(SoftDeleteModel):
 
     name = models.CharField(max_length=30)  # Entity name
     # entity_type = models.CharField(max_length=15, choices=ENTITY_TYPES, default="Single Entity Company")
-    entity_type = models.ForeignKey("EntityType", on_delete=models.SET_NULL, null =True)
+    entity_type = models.ForeignKey("core.EntityType", on_delete=models.SET_NULL, null =True)
     description = models.TextField()  # Description of the entity
-    instance = models.ForeignKey("Instance", on_delete=models.SET_NULL, blank=True, null=True, related_name="entities")  # Related instance
+    instance = models.ForeignKey("core.Instance", on_delete=models.SET_NULL, blank=True, null=True, related_name="entities")  # Related instance
     parent_entity = models.ForeignKey("self", on_delete=models.SET_NULL, blank=True, null=True)  # Optional parent entity for hierarchy
 
     def __str__(self):
