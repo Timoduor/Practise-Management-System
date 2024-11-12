fe0534df57d1f4e94fbc62e1a62746e78c4d36c8
Gethol <natenderitu@gmail.com>
2024-11-12 10:07:02 +0300

    feat: add children to entity serializer to allow for hierarchical fetching of entities

--- core/serializers/entity_serializers.py
+++ core/serializers/entity_serializers.py
@@ -6,13 +6,19 @@ from core.models.entity import Entity
 # Serializer for Entity model with nested UnitSerializer for related units
 class EntitySerializer(serializers.ModelSerializer, SoftDeleteMixin):
     units = UnitSerializer(many=True, read_only=True)  # Nested serializer for units within an entity
-
+    children = serializers.SerializerMethodField()
     class Meta:
         model = Entity
         fields = [
-            'id', 'name', 'entity_type', 'description', 'instance', 'parent_entity', 'is_deleted', 
+            'id', 'name', 'entity_type', 'description', 'instance','children' , 'parent_entity', 'is_deleted', 
             'created_at', 'updated_at', 'last_updated_by', 'created_by', "units"
         ]
+
+    def get_children(self, obj):
+        # Fetch child entities for the current entity
+        children = Entity.objects.filter(parent_entity=obj)
+        return EntitySerializer(children, many=True).data
+    
 
     def create(self, validated_data):
         user = self.context["request"]. user
