441bd69476d8372632c62711e5329d9f663682e1
Gethol <natenderitu@gmail.com>
2024-11-12 21:11:12 +0300

    feat: set up customised unit and entity types

--- core/serializers/entity_serializers.py
+++ core/serializers/entity_serializers.py
@@ -2,15 +2,19 @@ from rest_framework import serializers
 from .base_serializers import SoftDeleteMixin
 from .unit_serializers import UnitSerializer
 from core.models.entity import Entity
+from core.serializers.entity_type_serializers import EntityTypeSerializer
 
 # Serializer for Entity model with nested UnitSerializer for related units
 class EntitySerializer(serializers.ModelSerializer, SoftDeleteMixin):
     units = UnitSerializer(many=True, read_only=True)  # Nested serializer for units within an entity
+    entity_type = serializers.SerializerMethodField()  # Using SerializerMethodField for entity_type as name
     children = serializers.SerializerMethodField()
+    parent_entity = serializers.SerializerMethodField()
+    
     class Meta:
         model = Entity
         fields = [
-            'id', 'name', 'entity_type', 'description', 'instance','children' , 'parent_entity', 'is_deleted', 
+            'id', 'name','entity_type' , 'description', 'instance','children' , 'parent_entity' ,'parent_entity', 'is_deleted', 
             'created_at', 'updated_at', 'last_updated_by', 'created_by', "units"
         ]
 
@@ -19,6 +23,12 @@ class EntitySerializer(serializers.ModelSerializer, SoftDeleteMixin):
         children = Entity.objects.filter(parent_entity=obj)
         return EntitySerializer(children, many=True).data
     
+    def get_entity_type(self, obj):
+        # Retrieve the name of the entity type, if available
+        return obj.entity_type.name if obj.entity_type else None
+    
+    def get_parent_entity(self,obj):
+        return obj.parent_entity.name if obj.parent_entity else None
 
     def create(self, validated_data):
         user = self.context["request"]. user
--- core/serializers/unit_serializers.py
+++ core/serializers/unit_serializers.py
@@ -1,10 +1,13 @@
 from rest_framework import serializers
 from .base_serializers import SoftDeleteMixin
 from core.models.unit import Unit
+from core.serializers.unit_type_serializers import UnitTypeSerializer
 
 
 # Serializer for Unit model with SoftDeleteMixin
 class UnitSerializer(serializers.ModelSerializer, SoftDeleteMixin):
+    unit_type = serializers.SerializerMethodField()  # Using SerializerMethodField for entity_type as name
+
     class Meta:
         model = Unit
         # Define fields for Unit model
@@ -12,3 +15,6 @@ class UnitSerializer(serializers.ModelSerializer, SoftDeleteMixin):
             'id', 'name', 'unit_type', 'address', 'entity', 'is_deleted', 'created_at', 
             'updated_at', 'last_updated_by', 'created_by'
         ]
+
+    def get_unit_type(self,obj):
+        return obj.unit_type.name if obj.unit_type else None
